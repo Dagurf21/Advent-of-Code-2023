ac3722f2a5ce5c000e0fc3dc6d2a932e2d6ff080
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2023-12-16 11:33:56 +0000

    Initial Commit

---
+++ Day1-Trebuchet/trabuchet_part_2.py
@@ -0,0 +1,18 @@
+def open_file(filename):
+    lines_in_file = []
+    with open(filename, "r") as file:
+        for line in file:
+            line = line.strip()
+            lines_in_file.append(line)
+    return lines_in_file
+
+
+def main():
+    filename = "Day 1-Trebuchet/day_one_input.txt"
+    lines_in_file = open_file(filename)
+
+    print (lines_in_file)
+
+
+if __name__ == "__main__":
+    main()  
